841f4fa1cdb26cf108efeba07890348f48e3e4fc
Matthew Rhone <matthew.rhone@outlook.com>
2017-03-18 14:00:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Matt"
+  end
+
+  get '/hometown' do
+    "My hometown is Suisun"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Face of Melinda"
+  end
 end
