1fc2be7cf1ebb579c2cfe0fe23be09338dbf69b9
Chuan-Sheng Tai <johnsont426@gmail.com>
2017-03-21 08:12:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Johnson Tai"
+  end
+  get "/hometown" do
+    "My hometown is Tainan, Taiwan"
+  end
+  get "/favorite-song" do
+    "My favorite song is Yellow"
+  end
+
+
 end
