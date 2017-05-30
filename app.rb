a66622a12bd3e3b62fd2340f0d09914574815f49
Teresa Grosch <tgrosch91@gmail.com>
2017-05-30 02:10:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/name' do
+  "My name is Teresa."
+end
+get '/hometown' do
+  "My hometown is St. Louis"
+end
+get '/favorite-song' do
+  "My favorite song is Hello."
+end
 end
