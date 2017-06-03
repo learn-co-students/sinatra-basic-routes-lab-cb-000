4e03a885299dc96830132a19c8da927f2add90ab
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-03 10:54:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mohsen."
+  end
+
+  get '/hometown' do
+    "My hometown is the World"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Shake it off!'"
+  end
 end
