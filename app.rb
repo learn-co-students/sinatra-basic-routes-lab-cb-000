aababa804f623339c584d0a6954f64474f5a51e0
James Garcia <jgarcia@garciajames.com>
2017-06-09 12:45:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is James"
+  end
+
+  get '/hometown' do
+    "My hometown is Dallas"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Simple Man"
+  end
 end
