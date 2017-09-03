faa6a7990e251e8ffc516a8d422bb91c0cb4179a
Grant MacManus <flatiron@macdav.net>
2017-09-03 21:30:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "Hello. My name is Grant."
+  end
+
+  get '/hometown' do
+    "My hometown is Dallas, Texas."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Something I Can Never Have' by Nine Inch Nails."
+  end
+
 end
