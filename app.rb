d28ce2b3be447fe00fcc43f491506aef62ac6f68
Gino Bejarano <ginobejarano@gmail.com>
2017-09-14 06:10:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
-
 class App < Sinatra::Base
+
+  get '/name' do
+  'My name is Gino!'
+end
+
+
+get '/hometown' do
+  'My hometown is Fairfax!'
+end
+
+
+get '/favorite-song' do
+  'My favorite song is Restart!'
+end
 end
