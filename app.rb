f66ba9bdd8a29844da3a44d7218c798494449db2
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-22 22:22:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+    "Hello, World!"
+
+  end
+
+  get '/name' do
+    "My name is Xochitl"
+  end
+  get '/hometown' do
+    "My hometown is San Pedrito"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Gasolina"
+  end
+
 end
