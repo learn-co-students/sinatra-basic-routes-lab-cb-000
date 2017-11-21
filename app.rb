da08c9becf525fd1bf807b8b2c6c47c713e4b8a9
Gerald Ingraham Ingraham <gingraham@icloud.com>
2017-11-21 00:28:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+     "My name is Gerald Ingraham"
+   end
+   get '/hometown' do
+    "My hometown is Miami, FL."
+  end
+  get '/favorite-song' do
+   "My favorite song is 'This is a man's world.'"
+ end
 end
