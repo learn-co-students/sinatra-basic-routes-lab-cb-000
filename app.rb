7d2f662c290958f7308e6147e1877d44a5d41573
Michael Wahlin <wahlin@gmail.com>
2017-12-05 13:04:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Michael"
+  end
+
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is bacon lovers go to Seattle"
+  end
 end
