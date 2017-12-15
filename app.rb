a16586c7ea64d625078702fdd025d8ab1905ce28
Jorge Cossi <jorge_cossi@yahoo.com>
2017-12-15 17:55:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Jorge!"
+  end
+
+  get '/hometown' do
+    "My hometown is West New York, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Notorious Thugs by Notorious B.I.G featuring Bone Thugs-N-Harmony"
+  end
 end
