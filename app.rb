7ccd0514faf307ffc7275135697585f73e754a4b
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2018-01-02 23:41:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Zeeshan"
+  end
+
+  get '/hometown' do
+    "My hometown is Rawalpindi"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Yaousss"
+  end
 end
