3fb32b56cf1dd7f1a385b3339407659748bc8fec
Michael Anderson <michanders@msn.com>
2018-01-02 20:57:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    response = Rack::Response.new
+    response.status = 200
+    response.write "My name is John"
+  end
+
+  get '/hometown' do
+    response = Rack::Response.new
+    response.status = 200
+    response.write "My hometown is Johnville"
+  end
+
+  get '/favorite-song' do
+    response = Rack::Response.new
+    response.status = 200
+    response.write "My favorite song is Johnny and the Johns"
+  end
 end
