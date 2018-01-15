f5354f3aa25925a560b3d970250fe6b137355819
Marcus Havranek <marcus.havranek@havranek.co.at>
2018-01-15 13:09:00 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Marcus Havranek"
+  end
+
+  get '/hometown' do
+    "My hometown is Vienna, Austria"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Creep' by Radiohead"
+  end
 end
