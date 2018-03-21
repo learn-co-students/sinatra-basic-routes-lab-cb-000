0b9b736d71644907f5824d03937221e8dd82f530
Travis Clarkson <tk1667@gmail.com>
2018-03-21 00:05:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Travis"
+  end
+
+  get '/hometown' do
+    "My hometown is Minocqua"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Copperhead Road"
+  end
+
 end
