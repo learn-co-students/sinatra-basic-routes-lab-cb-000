53530cb06efc2743103b9d2a3172fada50d90412
Alec German <alec_german@yahoo.com>
2018-04-12 21:37:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Alec German"
+  end
+
+  get '/hometown' do
+    "My hometown is Pontiac, MI"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fallin by Alina Baraz"
+  end
 end
