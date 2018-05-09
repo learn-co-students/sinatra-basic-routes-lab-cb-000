848a8129b52416d0ea1b00e3d616bb05c0dda1da
michael hohl <mhohl@rocketmail.com>
2018-05-09 21:05:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Michael Hohl"
+  end
+
+  get '/hometown' do
+    "My hometown is rwc."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is #41 bt dmb."
+  end
+
 end
