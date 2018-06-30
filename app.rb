1955fca6034e929293a25c1c57776f78344e1384
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-30 19:43:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kirby"
+  end
+
+  get '/hometown' do
+    "My hometown is Lafayette"
+  end
+
+  get '/favorite-song' do
+    'My favorite song is "I Love Kanye"'
+  end
+
 end
