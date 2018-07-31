dbbe011735e9a0def8dd1050fdcd6a3e591c2a22
Adam Urbina <adamurbina@gmail.com>
2018-07-31 21:44:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    def call(env)
+        request = Rack::Request.new(env)
+        response = Rack::Response.new
+
+        route = request.path.split("/").last
+
+        response.write handle_request(route)
+
+        response.finish
+    end
+
+    def handle_request(route)
+        case route
+        when "name"
+            return "My name is Adam"
+        when "hometown"
+            return "My hometown is San Fracisco"
+        when "favorite-song"
+            return "My favorite song is California Love"
+        end
+    end
+
 end
