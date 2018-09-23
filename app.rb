c19824f3ce3163f0d692e76987e86a09641697ba
louis baptiste <louisvis@ymail.com>
2018-09-23 01:38:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+ get "/name" do
+   "My name is Louis "
+end
+
+get "/hometown" do
+  "My hometown is Miami "
+end
+
+get "/favorite-song" do
+  "My favorite song is Lose yourself "
+end
+
+
 end
