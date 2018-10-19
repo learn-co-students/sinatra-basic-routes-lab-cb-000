d2a3992d9c92d8842fc8220f6a751e6e05d27ef9
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-19 00:33:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kenneth R. Kutschera"
+  end
+
+  get '/hometown' do
+    "My hometown is Albany, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Float On by Modest Mouse"
+  end
 end
