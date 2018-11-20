408347575bd6c6bb682a69a10e32c12752583310
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 07:56:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Hunter"
+  end
+  
+  get '/hometown' do
+    "My hometown is Glendora"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is THE HOUSE OF THE RISING SUN"
+  end
+  
 end
