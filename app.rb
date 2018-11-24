4549a85977da7a9e6bedcfe38173362f8ef653ac
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-11-24 21:59:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Yosia"
+  end 
+  
+  get '/hometown' do 
+    'My hometown is Jakarta'
+  end 
+  
+  get '/favorite-song' do
+    'My favorite song is KKEB'  
+  end
 end
