d0df4953708df691088b2017744b841e74696630
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-11-29 11:49:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Sergio"
+  end
+
+  get '/hometown' do
+    "My hometown is Newtown"
+  end
+
+
+  get '/favorite-song' do
+    "My favorite song is mmmmmm"
+  end
+
+
 end
