22768b6c7a32d7499f37e751ee28cbd7e615fcb0
Fer Hernandez <fh.1111@gmail.com>
2018-12-29 21:50:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Fer"
+  end
+
+  get '/hometown' do
+    "My hometown is SC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Guerras Perdidas"
+  end
+  
 end
