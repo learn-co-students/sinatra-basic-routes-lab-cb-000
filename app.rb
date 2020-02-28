390241739c01052ca229b8315c6ebcd706ac914b
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-28 20:24:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Mobola."
+  end
+  
+  get '/hometown' do
+    "My hometown is Alakuko."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Shimbalae."
+  end
+  
 end
